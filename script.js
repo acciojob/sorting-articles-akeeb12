237946e2f5c0a14a1fc7e6f138553666067e7204
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-16 18:57:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const band = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 function strip(bands) {
-	return bands.replace(/^(a |an |the )/i, '').trim();
+	return band.replace(/^(a |an |the )/i, '').trim();
 }
-const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
+const sortedBands = band.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
 const bandList = document.querySelector('#band');
-bandList.innerHTML = sortedBands.map(bands =>`<li>${band}</li>`).join('');
+bandList.innerHTML = sortedBands.map(band =>`<li>${band}</li>`).join('');
